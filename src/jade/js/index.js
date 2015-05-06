9a7e4457fff82a2165cbdbf2a1095aa17380648e
seaman <semen.bko@ya.ru>
2015-05-06 12:15:14 +0300

    contact form and map

--- src/jade/js/index.js
+++ src/jade/js/index.js
@@ -76,7 +76,6 @@ $(document).ready(function(){
 	$navButton = $('.nav-button i');
 	$navBefore = $('.nav-before');
 	$navLogo = $('.nav-logo');
-	console.log(isMobile(960));
 	$window.scroll(function(){
 		if($window.scrollTop() > $('.landing__info').height()){
 			if(!isMobile(960)){
@@ -90,6 +89,8 @@ $(document).ready(function(){
 			$navLogo.hide();
 		}
 	});
+
+	$('.map-container').height($('.contact-form').height())
 
 	$('.fb-resize').autoResizeFbPost();
 
@@ -139,3 +140,20 @@ $(document).ready(function(){
 		autoHeightLanging();
 	});
 });
+
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+	 var mapOptions = {
+		zoom: 13,
+		center: new google.maps.LatLng(40.7747151, -73.9567418),
+		styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
+	};
+	var mapElement = document.querySelector('.map-container');
+	var map = new google.maps.Map(mapElement, mapOptions);
+	var marker = new google.maps.Marker({
+		position: new google.maps.LatLng(40.7747151, -73.9567418),
+		map: map,
+		title: 'Shaggy hair studio'
+	});
+}
